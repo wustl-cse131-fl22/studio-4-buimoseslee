716c85de691c9d5f3785f5f855e1d98c359a1e67
Ethan Moses <ethanmoses04@gmail.com>
2022-10-12 16:58:50 -0500

    flag ex

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,10 +6,12 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.filledRectangle(0, 0, 1, 0.5);
 		StdDraw.setPenColor(51, 204, 255);
-		StdDraw.filledRectangle(0, 0, 10, 5);
+		StdDraw.filledRectangle(0.1, 0.1, 0.875, 0.375);
 		StdDraw.setPenColor(255, 255, 0);
 		StdDraw.setPenRadius(0.5);
-		StdDraw.filledCircle(10, 5, 3);
+		StdDraw.filledCircle(0.5, 0.25, 0.15);
 	}
 }
